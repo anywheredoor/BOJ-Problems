23245daa010345c6b4f127545536c8cf59775dce
mini-dora <121270060+mini-dora@users.noreply.github.com>
2024-03-18 20:24:56 +0900

    Update 2839.py

--- Greedy/2839.py
+++ Greedy/2839.py
@@ -1,1 +1,32 @@
+"""
+https://www.acmicpc.net/problem/2839 (설탕 배달)
 
+Mathematics
+Dynamic Programming
+Greedy
+
+Solved with Greedy
+"""
+
+import sys
+
+N = int(sys.stdin.readline())
+
+if N % 5 == 0:
+    print(N//5)
+else:
+    cnt = 0
+    
+    while N >= 3:
+        N -= 3
+        cnt += 1
+
+        if N == 0:
+            print(cnt)
+            break
+
+        if N % 5 == 0:
+            print(cnt + N//5)
+            break
+    else:
+        print(-1)
