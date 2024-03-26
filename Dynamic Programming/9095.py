c7d3d8e64cb532ed04fce1e87b887a577a420f7d
WONJUN YANG <121270060+mini-dora@users.noreply.github.com>
2024-03-26 20:04:54 +0900

    Create 9095.py

---
+++ Dynamic Programming/9095.py
@@ -0,0 +1,21 @@
+"""
+https://www.acmicpc.net/problem/9095 (1, 2, 3 더하기)
+
+Dynamic Programming
+"""
+
+import sys
+
+dp = [0]*11
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in range(4, 11):
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    n = int(sys.stdin.readline())
+    print(dp[n])
