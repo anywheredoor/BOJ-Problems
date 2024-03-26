0174dbba388993fd8c9840147f1c0c4a07bf594a
WONJUN YANG <121270060+mini-dora@users.noreply.github.com>
2024-03-26 19:48:03 +0900

    Create 1463.py

---
+++ Dynamic Programming/1463.py
@@ -0,0 +1,22 @@
+"""
+https://www.acmicpc.net/problem/1463 (1로 만들기)
+
+Dynamic Programming
+"""
+
+import sys
+
+N = int(sys.stdin.readline())
+
+dp = [0] * (N+1)
+
+for i in range(2, N+1):
+    dp[i] = dp[i-1] + 1
+
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2] + 1)
+
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3] + 1)
+
+print(dp[N])
