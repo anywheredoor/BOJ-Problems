448541ed47b83276e38a7a709481dd26f3cd7f27
anywheredoor <121270060+anywheredoor@users.noreply.github.com>
2024-04-22 14:14:25 +0900

    Create 1715 - Priority Queue.py

---
+++ Sorting/1715 - Priority Queue.py
@@ -0,0 +1,27 @@
+"""
+https://www.acmicpc.net/problem/1715 (카드 정렬하기)
+
+Data Structures
+Greedy
+Priority Queue
+"""
+
+import heapq
+
+n = int(input())
+
+heap = []
+for i in range(n):
+    data = int(input())
+    heapq.heappush(heap, data)
+
+result = 0
+
+while len(heap) != 1:
+    one = heapq.heappop(heap)
+    two = heapq.heappop(heap)
+    sum_value = one + two
+    result += sum_value
+    heapq.heappush(heap, sum_value)
+
+print(result)
