ecf3c8de3580cc6a81a726622fcdda4d55950388
anywheredoor <121270060+anywheredoor@users.noreply.github.com>
2024-05-07 18:47:08 +0900

    Create 18353 - another approach.py

---
+++ Dynamic Programming/18353 - another approach.py
@@ -0,0 +1,19 @@
+"""
+https://www.acmicpc.net/problem/18353 (병사 배치하기)
+
+Dynamic Programming
+Longest Increasing Sequence In O(n Log N)
+"""
+
+n = int(input())
+array = list(map(int, input().split()))
+array.reverse()
+
+dp = [1] * n
+
+for i in range(1, n):
+    for j in range(0, i):
+        if array[j] < array[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(n - max(dp))
