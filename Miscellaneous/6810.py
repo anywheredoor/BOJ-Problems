61f5e616f3dae908d41027b1e1e32ac2c8952033
WonJun YANG <121270060+anywheredoor@users.noreply.github.com>
2024-06-17 17:22:03 +0900

    Create 6810.py

---
+++ Miscellaneous/6810.py
@@ -0,0 +1,14 @@
+"""
+https://www.acmicpc.net/problem/6810 (ISBN)
+
+Mathematics
+Implementation
+Arithmetic
+"""
+first = int(input())
+second = int(int(input()) * 3)
+third = int(input())
+
+ans = 91 + first + second + third
+
+print(f"The 1-3-sum is {ans}")
