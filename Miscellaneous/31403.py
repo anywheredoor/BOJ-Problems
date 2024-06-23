87cc1aa967edf6361347eeadb40ca59e237fb78d
WonJun YANG <121270060+anywheredoor@users.noreply.github.com>
2024-06-23 12:58:12 +0900

    Create 31403.py

---
+++ Miscellaneous/31403.py
@@ -0,0 +1,16 @@
+"""
+https://www.acmicpc.net/problem/31403
+
+Mathematics
+String
+Arithmetic
+"""
+A = input()
+B = input()
+C = input()
+
+ans_1 = int(A) + int(B) - int(C)
+ans_2 = int(A + B) - int(C)
+
+print(ans_1)
+print(ans_2)
