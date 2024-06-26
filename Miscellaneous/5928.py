c85981cda787be18bb3914418eb10038bc0017fd
WonJun YANG <121270060+anywheredoor@users.noreply.github.com>
2024-06-26 17:26:27 +0900

    Create 5928.py

---
+++ Miscellaneous/5928.py
@@ -0,0 +1,18 @@
+"""
+https://www.acmicpc.net/problem/5928 (Contest Timing)
+
+Mathematics
+Implementation
+Arithmetic
+"""
+D, H, M = map(int, input().split())
+
+t1 = D * 24 * 60 + H * 60 + M
+t2 = 11 * 24 * 60 + 11 * 60 + 11
+
+t = t1 - t2
+
+if t < 0:
+    print(-1)
+else:
+    print(t)
